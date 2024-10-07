9bed581bcf36e9d4079809419e8010c561d635b6
Gabriella Bevilacqua <bevi.gaby@gmail.com>
2024-10-07 19:03:12 +0000

    generando carta con boton

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
+import { suit, values } from "./cards";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const showCard = (randomSuit, randomValues) => {
+  const card = document.querySelector("#card");
+  card.innerHTML = `
+    <div>${randomSuit}<div/>
+    <div>${randomValues}<div/>
+    <div>${randomSuit}<div/>`;
+};
+
+const generateRandomCard = () => {
+  const randomSuit = suit[Math.floor(Math.random() * 4)];
+  const randomValues = values[Math.floor(Math.random() * 13)];
+  showCard(randomSuit, randomValues);
+};
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  generateRandomCard();
+
+  document
+    .querySelector("#btn-newCard")
+    .addEventListener("click", () => generateRandomCard());
 };
